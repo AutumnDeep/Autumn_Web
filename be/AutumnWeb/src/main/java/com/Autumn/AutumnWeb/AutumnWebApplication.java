ac01a7053fafea143014557af8ea2fbb8b43bfbd
DESKTOP-J8KSOOE\rlarl <rlarlgus300@gmail.com>
2023-09-11 18:47:28 +0900

    fe, be 초기 환경 설정

---
+++ be/AutumnWeb/src/main/java/com/Autumn/AutumnWeb/AutumnWebApplication.java
@@ -0,0 +1,13 @@
+package com.Autumn.AutumnWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutumnWebApplication.class, args);
+	}
+
+}
---
+++ be/AutumnWeb/src/test/java/com/Autumn/AutumnWeb/AutumnWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Autumn.AutumnWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutumnWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
