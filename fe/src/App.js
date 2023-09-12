2432c07270c9c4ea653182833b11c417304b5781
DESKTOP-J8KSOOE\rlarl <rlarlgus300@gmail.com>
2023-09-13 03:04:22 +0900

    1. 테일윈드에서 twin-Macro 적용 1.1 twin-Macro = 기존의 테일윈드 적용시 className이 더러워지는걸 방지해서 나온 방식, css방식도 가능

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,12 +1,20 @@
-import ProductList from "./Web/ProductList";
-import "./index.css";
+import tw, { css, styled } from 'twin.macro';
+import ProductList from './Web/ProductList';
+
+const MyComponent = styled.div`
+  ${tw`bg-blue-500 p-4 font-bold`} // 테일윈드 방법
+  ${css`color: red;`} // css 방법 
+`;
 
 function App() {
-    return (
-        <div>
-          <ProductList />
-        </div>
-    );
+  return (
+    <div>
+      <MyComponent > {/* 기존 방식도 가능*/}
+        Styled with twin.macro and Emotion
+      </MyComponent>
+      <ProductList />
+    </div>
+  );
 }
 
 export default App;
